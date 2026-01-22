6e357f6b583375ee75a82b6c43f77a3ba97a2aa5
spirosmesa <spirosmesa@yahoo.gr>
2026-01-22 09:43:01 +0100

    minor bug fixing

--- cargo_audit_parser/col_descriptors.py
+++ cargo_audit_parser/col_descriptors.py
@@ -15,9 +15,9 @@ def get_cargo_vulnerabilities_col_descriptions() -> list[Tuple[str, str]]:
             ("Description", "A description of the vulnerability."),
             ("Package Source (URL)", "The URL of the package. Most frequently a GH link."),
             ("Date First Discovered", "The date the vulnerability was first discovered in the wild."),
-            ("Vulnerability Aliases", "A list of any known vulnerability aliases, seperated by `\n`."),
-            ("Vulnerability Categories", "A list of categories that fit vulnerability, seperated by `\n`"),
-            ("Vulnerability References", "Any vulnerability references seperated by `\n`"),
+            ("Vulnerability Aliases", "A list of any known vulnerability aliases, seperated by a newline."),
+            ("Vulnerability Categories", "A list of categories that fit vulnerability, seperated by a newline"),
+            ("Vulnerability References", "Any vulnerability references seperated by a newline"),
             ("Vulnerability Source", "Registry source where the vulnerability is created."),
             ("Vulnerability URL", "URL of the security advisory, if any exists."),
             ("Vulnerability Withdrawn", "Whether or not the vulnerability has been withdrawn"),
@@ -34,9 +34,9 @@ def get_cargo_affected_vulnerabilities_col_descriptions() -> list[Tuple[str, str
     :rtype: list[Tuple[str, str]]
     """
     return [
-        ("Affected Architecture", "A list of any known affected architectures, delimited by `\n`."),
-        ("Affected OS(es)", "A list of any known affected OSes, delimited by `\n`."),
-        ("Affected Functions", "A list of any known affected functions, delimited by `\n`."),
+        ("Affected Architecture", "A list of any known affected architectures, delimited by a newline."),
+        ("Affected OS(es)", "A list of any known affected OSes, delimited by a newline."),
+        ("Affected Functions", "A list of any known affected functions, delimited by a newline."),
     ]
 
 def get_cargo_all_col_descriptions() -> list[Tuple[str, str]]:
